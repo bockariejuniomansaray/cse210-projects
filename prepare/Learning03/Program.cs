a47056887a87925e8123c056b567d516bbb6fabd
bockariejuniormansaray <magnificentj8@gmail.com>
2023-06-24 08:42:28 +0000

    my fraction working

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,9 +1,9 @@
-using System;
-
 class Program
 {
+    /// <summary>
+    /// The main entry point for the program.
+    /// </summary>
+    /// <param name="args"></param>
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
-    }
-}
+        Fraction f1 = new Fraction();
