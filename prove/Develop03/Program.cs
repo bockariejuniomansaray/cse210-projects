570dead592241065800cdc45ef76ad1142bcf13a
bockariejuniormansaray <magnificentj8@gmail.com>
2023-06-30 12:08:26 +0000

    week 6

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,30 +1,97 @@
+using System;
+using System.Collections.Generic;
+
 class Program
 {
-    static void Main()
+    static void Main(string[] args)
     {
-        // Initialize the scripture
-        var scripture = new Scripture("John 3:16", "For God so loved the world that he gave his one and only Son, that whoever believes in him shall not perish but have eternal life.");
+        Scripture scripture = new Scripture("John 3:16", "For God so loved the world, that he gave his only Son, that whoever believes in him should not perish but have eternal life.");
+        scripture.Display();
 
-        // Display the complete scripture
-        Console.WriteLine(scripture.ToString());
-
-        // Prompt the user
-        Console.WriteLine("Press Enter to hide words or type 'quit' to end.");
-
-        while (!scripture.AreAllWordsHidden())
+        while (scripture.HasHiddenWords())
         {
-            string input = Console.ReadLine().Trim().ToLower();
+            Console.WriteLine("Press ENTER to continue or type 'quit' to exit.");
+            string input = Console.ReadLine();
 
-            if (input == "quit")
+            if (input.ToLower() == "quit")
                 break;
 
-            // Hide a few random words and display the updated scripture
-            scripture.HideRandomWords(2);
-            Console.Clear();
-            Console.WriteLine(scripture.ToString());
-            Console.WriteLine("Press Enter to hide words or type 'quit' to end.");
+            scripture.HideRandomWord();
+            scripture.Display();
+        }
+    }
+}
+
+class Scripture
+{
+    private string reference;
+    private string text;
+    private List<Word> words;
+    private Random random;
+
+    public Scripture(string reference, string text)
+    {
+        this.reference = reference;
+        this.text = text;
+        this.words = new List<Word>();
+        this.random = new Random();
+
+        string[] wordArray = text.Split(' ');
+
+        foreach (string wordText in wordArray)
+        {
+            Word word = new Word(wordText);
+            words.Add(word);
+        }
+    }
+
+    public void Display()
+    {
+        Console.Clear();
+        Console.WriteLine(reference);
+        Console.WriteLine();
+
+        foreach (Word word in words)
+        {
+            if (word.IsHidden)
+                Console.Write("***** ");
+            else
+                Console.Write(word.Text + " ");
         }
 
-        Console.WriteLine("Program ended.");
+        Console.WriteLine("\n");
+    }
+
+    public void HideRandomWord()
+    {
+        List<Word> visibleWords = words.FindAll(word => !word.IsHidden);
+
+        if (visibleWords.Count > 0)
+        {
+            int index = random.Next(visibleWords.Count);
+            visibleWords[index].Hide();
+        }
+    }
+
+    public bool HasHiddenWords()
+    {
+        return words.Exists(word => word.IsHidden);
+    }
+}
+
+class Word
+{
+    public string Text { get; }
+    public bool IsHidden { get; private set; }
+
+    public Word(string text)
+    {
+        Text = text;
+        IsHidden = false;
+    }
+
+    public void Hide()
+    {
+        IsHidden = true;
     }
 }
